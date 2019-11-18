e36874b6712ccd6b57d08152885fa3bf280f54b8
ChaseConover <48431884+ChaseConover@users.noreply.github.com>
2019-11-17 22:33:34 -0500

    Add files via upload

---
+++ Song.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+
+/**
+ * @author Chase Conover
+ * @version 2019.11.17
+ *
+ */
+public class Song {
+    private String title;
+    private int year;
+    private String genre;
+    private String artist;
+    
+    /**
+     * creates a new song object
+     * @param title is the title of the song
+     * @param year is the release year
+     * @param genre is the song genre
+     * @param artist is the artist of the song
+     */
+    public Song(String title, int year, String genre, String artist)
+    {
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.artist = artist;
+    }
+    
+    /**
+     * 
+     * @return the title of the song
+     */
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    /**
+     * 
+     * @return the release year of the song
+     */
+    public int getYear()
+    {
+        return year;
+    }
+    
+    /**
+     * 
+     * @return the genre of the song
+     */
+    public String getGenre()
+    {
+        return genre;
+    }
+    
+    /**
+     * 
+     * @return the artist of the song
+     */
+    public String getArtist()
+    {
+        return artist;
+    }
+    
+    /**
+     * @return true if the objects are equal
+     */
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        else if ( obj == null)
+        {
+            return false;
+        }
+        else if(this.getClass() == obj.getClass())
+        {
+            Song temp = (Song) obj;
+            return this.toString().equals(temp.toString());
+        }
+        return false;
+    }
+    
+    /**
+     * @return the Song as a string
+     */
+    public String toString()
+    {
+        StringBuilder build = new StringBuilder();
+        build.append("Song Title: "+ title);
+        build.append("\nSong Artist: " + artist);
+        build.append("\nSong Genre: " + genre);
+        build.append("\nSong Year: " + year);
+        return build.toString();
+    }
+    
+
+}
---
+++ SongTest.java
@@ -0,0 +1,56 @@
+import student.TestCase;
+
+/**
+ * 
+ */
+
+/**
+ * @author Chase Conover
+ * @veriosn 2019.11.17
+ *
+ */
+public class SongTest extends TestCase {
+    private Song song;
+    private Song equals;
+    public void setUp()
+    {
+        song = new Song("B", 2019, "Rap", "Chase");
+        equals = new Song("B", 2019, "Rap", "Chase");
+    }
+    /**
+     * tests getters
+     */
+    public void testGetter()
+    {
+        assertEquals(song.getArtist(), "Chase");
+        assertEquals(song.getGenre(), "Rap");
+        assertEquals(song.getTitle(), "B");
+        assertEquals(song.getYear(), 2019);
+    }
+    /**
+     * tests equals
+     */
+    public void testEquals()
+    {
+        //when the objects are the same
+        assertTrue(song.equals(song));
+        //when the obj is null
+        Song nullSong = null;
+        assertFalse(song.equals(nullSong));
+        //when the songs are in the same class and =
+        assertTrue(song.equals(equals));
+        //when obj is in the same class and !=
+        Song notEqual = new Song("A", 2017, "Rock", "Joe");
+        assertFalse(song.equals(notEqual));
+        //when obj is not in the same class
+        Object obj = new Object();
+        assertFalse(song.equals(obj));
+    }
+    /**
+     * tests toString
+     */
+    public void testToString()
+    {
+        assertEquals(song.toString(), "Song Title: B\nSong Artist: Chase\nSong Genre: Rap\nSong Year: 2019");
+    }
+}
